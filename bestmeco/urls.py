c99f30473ed0659c897cbf50ae8be15cd0c4ed3e
franckopheno <ngamofrancois527@gmail.com>
2025-04-28 01:41:36 +0100

    fixer le bug de l'affichage des produits

--- bestmeco/urls.py
+++ bestmeco/urls.py
@@ -17,6 +17,8 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path,include
 from django.contrib import admin
+from django.conf import settings
+from django.conf.urls.static import static
 
 admin.site.site_header = "meco Administration"
 admin.site.site_title = "meco Admin"
@@ -28,3 +30,5 @@ urlpatterns = [
      path('', include('apps.core.urls')),  # Ajout des URLs de l'application core
 
 ]
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
